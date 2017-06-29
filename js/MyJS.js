8605483519dbf69a0b326941e1d6a18c473841f1
phucv <phucv@DESKTOP-GI5FGLR>
2017-06-29 15:26:10 +0700

    update categories, about

--- js/MyJS.js
+++ js/MyJS.js
@@ -21,3 +21,29 @@
         }
     });
 }());
+
+//categories
+$(document).ready(function () {
+    var navItems = $('.admin-menu li > a');
+    var navListItems = $('.admin-menu li');
+    var allWells = $('.admin-content');
+    var allWellsExceptFirst = $('.admin-content:not(:first)');
+
+    allWellsExceptFirst.hide();
+    navItems.click(function (e) {
+        e.preventDefault();
+        navListItems.removeClass('active');
+        $(this).closest('li').addClass('active');
+
+        allWells.hide();
+        var target = $(this).attr('data-target-id');
+        $('#' + target).show();
+    });
+});
+
+//search by prices
+$("#ex13").slider({
+    ticks: [0, 100, 200, 300, 400],
+    ticks_labels: ['$0', '$100', '$200', '$300', '$400'],
+    ticks_snap_bounds: 30
+});
